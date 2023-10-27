abf3037a4584439a7ed405563ebdaddede822e54
zrpy <91083604+zrpy@users.noreply.github.com>
2023-10-27 18:01:07 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import openai,discord
+openai.api_key="sk-xxx"
+openai.api_base="https://api.chatanywhere.cn/v1"
+client=discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!gpt "):
+        async with message.channel.typing():
+            await message.channel.send("GPT-3.5:"+openai.ChatCompletion.create(model='gpt-3.5-turbo',messages=[{'role': 'user', 'content': message.content.split("!gpt ")[1]}])["choices"][0]["message"]["content"])
+
+client.run("Token")
